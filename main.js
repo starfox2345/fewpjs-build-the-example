d07700d232d325488c6b92ea0db471658db3c241
yatsun <yatsunlam25@gmail.com>
2021-09-06 01:08:57 -0400

    Done.

--- main.js
+++ main.js
@@ -4,6 +4,39 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", function(){
+  let myModal = document.getElementById("modal");
+  myModal.classList.add("hidden");
+  likeButton();
+})
+
+function likeButton(){
+  let likeBtn = document.querySelectorAll('.like-glyph');
+
+  likeBtn.forEach(trigger => trigger.addEventListener('click', function(event){
+    mimicServerCall()
+      .then(displayLikes(event)
+      )
+      .catch(error => {
+        let myModal = document.getElementById("modal");
+        myModal.classList.remove("hidden");
+
+        let myModalMessage = document.getElementById("modal-message");
+        myModalMessage.innerText = error;
+        setTimeout(() => myModal.classList.add("hidden"), 3000);
+      })
+  }))
+}
+
+function displayLikes(event){
+  if (event.target.innerText == '♡'){
+    event.target.innerText = FULL_HEART;
+    event.target.classList.add("activated-heart");
+  } else if (event.target.innerText == '♥'){
+    event.target.innerText = EMPTY_HEART;
+    event.target.classList.remove("activated-heart");
+  }
+}
 
 
 
